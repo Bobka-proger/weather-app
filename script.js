46c0b9bd828c971a9d4269919241b88c22b2f8bf
Bobka-proger <ellaebel950@gmai.com>
2025-03-22 20:31:49 +0700

    погода с городами

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-const API_WEATHER = "https://api.openweathermap.org/data/2.5/weather?lat=55.7522&lon=37.6156&units=metric&lang=ru&appid=0e5ceefe1b8038f58348f443896ad4c7"
 
 let weatherInfo = document.querySelector("#weatherInfo")
 
-async function getWeather() {
+async function getWeather(lat, lon) {
+    const API_WEATHER = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&lang=ru&appid=0e5ceefe1b8038f58348f443896ad4c7`
     const resp = await fetch(API_WEATHER)
     const respToJSON = await resp.json()
+    let name = respToJSON.name
     let {temp} = respToJSON.main
     let {description, icon} = respToJSON.weather[0]
+    let h1 = document.querySelector("h1")
     weatherInfo.innerHTML = `
+    <h1 class="text-center my-5">погода in ${name}</h1>
     <img class="weather-icon" src="http://openweathermap.org/img/wn/${icon}@4x.png">
     <p class="temperature">${Math.round(temp)}°</p>
     <p class="description">${description}</p>
+
     `
     let body = document.body
 
@@ -37,6 +41,16 @@ async function getWeather() {
             body.classList = "windy"
             break;
     }
-    console.log(description)
 }
-getWeather()
+
+function getlocation () {
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            let lat = position.coords.latitude
+            let lon = position.coords.longitude
+            getWeather(lat, lon)
+        }
+    )
+}
+
+getlocation()
